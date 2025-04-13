b369a9e87cfac63f24773dbe173419b346ac722f
guziczak <guziczak@proton.me>
2025-04-13 09:37:26 +0200

    Dodano wpis pozwalający na przejście testu AddressDao

--- wsb-jpa1/src/main/resources/data.sql
+++ wsb-jpa1/src/main/resources/data.sql
@@ -2,6 +2,9 @@ INSERT INTO ADDRESS (city, addressLine1, addressLine2, postalCode)
 VALUES ('Warsaw', 'Street 1', 'Apt 10', '00-001'),
 ('Kraków', 'ul. Floriańska 5', NULL, '31-019');
 
+INSERT INTO ADDRESS (id, city, addressLine1, addressLine2, postalCode)
+VALUES (901, 'Wroclaw', 'ul. Testowa 55', NULL, '60-400');
+
 INSERT INTO DOCTOR (firstName, lastName, telephoneNumber, email, doctorNumber, specialization, address_id)
 VALUES ('John', 'Doe', '123456789', 'john.doe@example.com', 'D123', 'SURGEON', 1);
 
